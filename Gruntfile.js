36ad6dfc1194be3aceaadb7471b3d46e92d71540
Dan Hable <dhable@gmail.com>
2013-10-29 19:17:36 -0700

    copying bower modules to the public location

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,19 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       files: ["Gruntfile.js", "app.js", "public/**/*.js"]
+    },
+    copy: {
+      main: {
+        files: [
+          {src: ["bower_components/**"], dest: "public/"}
+        ]
+      }
     }
   });
 
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-bower-install-task");
+  grunt.loadNpmTasks("grunt-contrib-copy");
 
-  grunt.registerTask("default", ["jshint", "bower_install"]);
+  grunt.registerTask("default", ["jshint", "bower_install", "copy"]);
 };
