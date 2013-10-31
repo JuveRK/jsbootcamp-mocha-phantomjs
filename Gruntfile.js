8d8eceb1adcf127bfc1fe55b7a10053d493c7574
Dan Hable <dhable@gmail.com>
2013-10-30 17:12:30 -0700

    Injecting Mocha-PhantomJS plugin into grunt's control.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,13 +10,23 @@ module.exports = function(grunt) {
         ]
       }
     },
-    clean: ["bower_components", "public/bower_components", "node_modules"]
+    clean: ["bower_components", "public/bower_components", "node_modules"],
+    mocha_phantomjs: {
+      all: ["tests/**/*.html"],
+      options: {
+        reporter: "xunit",
+        output: "./mocha_result.xml"
+      }
+    }
   });
 
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-bower-install-task");
   grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-mocha-phantomjs");
+
 
   grunt.registerTask("default", ["jshint", "bower_install", "copy"]);
+  grunt.registerTask("test", ["mocha_phantomjs"]);
 };
