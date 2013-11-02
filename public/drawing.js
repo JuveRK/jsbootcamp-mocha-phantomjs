c2ca364267c2eadacea13621dd947706c754e169
Ryan Juve <RKJuve@gmail.com>
2013-11-02 15:48:19 -0700

    can draw circles on mousedown. 'stamp tool' also raphael canvas resizes with window

--- public/drawing.js
+++ public/drawing.js
@@ -2,7 +2,12 @@
 // javascript and jquery to run simple drawing app using raphael    also bootstrap
 
 $(document).ready(function(){
-	var paper = Raphael(0, 0, 800, 800);
-	var circle = paper.circle(50, 40, 20);
-	circle.attr("fill", "#385");
+	var paper = Raphael(0, 0, window.innerWidth, window.innerHeight);
+	$(window).resize(function(){
+		paper.setSize(window.innerWidth, window.innerHeight)
+	});
+	$('svg').mousedown(function(event){
+		var circle = paper.circle(event.pageX, event.pageY, 20);
+		circle.attr("fill", "#385");
+	});
 });
