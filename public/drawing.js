4f0ae49f8503810c60f10c2cf133efca8746855f
Ryan Juve <RKJuve@gmail.com>
2013-11-02 16:54:46 -0700

    basic stroke drawing functionality

--- public/drawing.js
+++ public/drawing.js
@@ -2,12 +2,46 @@
 // javascript and jquery to run simple drawing app using raphael    also bootstrap
 
 $(document).ready(function(){
-	var paper = Raphael(0, 0, window.innerWidth, window.innerHeight);
+	var paper = new Raphael(0, 0, window.innerWidth, window.innerHeight);
 	$(window).resize(function(){
 		paper.setSize(window.innerWidth, window.innerHeight)
 	});
+
+	// stamp tool
+	/*
 	$('svg').mousedown(function(event){
 		var circle = paper.circle(event.pageX, event.pageY, 20);
 		circle.attr("fill", "#385");
+	});
+	*/
+
+	// stroke tool
+
+	var mousedown = false
+
+	$('svg').mousedown(function(event){
+	var lastX, lastY, pathString, path;
+	    mousedown = true;
+
+	    var x = event.pageX,
+	        y = event.pageY;
+
+	    pathString = 'M' + x + ',' + y + 'l0,0';
+    	path = paper.path(pathString).attr({"fill": "none", stroke: "#000", "stroke-width": 5});	
+	    lastX = x;
+	    lastY = y;
+		$(document).mouseup(function(){
+	    	mousedown = false;
+		});
+		$('svg').mousemove(function(event){
+	    	if (!mousedown) {
+	        	return;
+	    	}
+		    var x = event.pageX,
+		        y = event.pageY;
+	    	pathString += 'L' + x + ',' + y; 
+	    	$('path').attr({"d":pathString});
+		});
+
 	});
 });
