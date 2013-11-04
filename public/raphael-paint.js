f525ccdbdc46ab734fa16892a1b80e8dc66803e0
Ryan Juve <RKJuve@gmail.com>
2013-11-03 23:32:55 -0800

    toolbar styling, fill color input added, start of shape tool

--- public/raphael-paint.js
+++ public/raphael-paint.js
@@ -14,14 +14,19 @@ $(document).ready(function(){
 		paper.setSize(window.innerWidth, window.innerHeight)
 	});
 	// set up global variables for drawing attributes
+	var strokeNumber = 0;
 	var strokeColor = $('#strokeColorPicker').val();
 	var strokeWidth = $('#strokeWidthPicker').val();
+	var fillColor = $('#fillColorPicker').val();
 	// link drawing attributes to toolbar inputs
 	$('#strokeColorPicker').change(function(){
 		strokeColor = this.value;
 	});
 	$('#strokeWidthPicker').change(function(){
 		strokeWidth = this.value;
+	});
+	$('#fillColorPicker').change(function(){
+		fillColor = this.value;
 	});
 	// give buttons event watchers and fire their functions
 	$('.clear:button').click(function(){
@@ -40,36 +45,72 @@ $(document).ready(function(){
 	});
 	*/
 
-	// stroke tool
+	// path tool 
 
-	var mousedown = false;
-	var strokeNumber = 0;
-	$('svg').mousedown(function(event){
-	var lastX, lastY, pathString, path;
-		event.preventDefault(); //cursor fix
-		strokeNumber++;
-	    mousedown = true;
 
-	    var x = event.pageX,
-	        y = event.pageY;
 
-	    pathString = 'M' + x + ',' + y + 'l0,0';
-    	path = paper.path(pathString).attr({"fill": "none", "stroke-linecap": "round", "stoke-linejoin": "round", stroke: strokeColor, "stroke-width": strokeWidth});	
-    	$('path:not([id])').attr("id",strokeNumber);
-	    lastX = x;
-	    lastY = y;
-		$(document).mouseup(function(){
-	    	mousedown = false;
-		});
-		$('svg').mousemove(function(event){
-	    	if (!mousedown) {
-	        	return;
-	    	}
+		var mousedown = false;
+		$('svg').mousedown(function(event){
+		var lastX, lastY, pathString, path;
+			event.preventDefault(); //cursor fix
+			strokeNumber++;
+		    mousedown = true;
+
 		    var x = event.pageX,
 		        y = event.pageY;
-	    	pathString += 'L' + x + ',' + y; 
-	    	$("#"+strokeNumber).attr({"d":pathString});
-		});
 
-	});
+		    pathString = 'M' + x + ',' + y + 'l0,0';
+	    	path = paper.path(pathString).attr({"fill": "none", "stroke-linecap": "round", "stoke-linejoin": "round", stroke: strokeColor, "stroke-width": strokeWidth});	
+	    	$('path:not([id])').attr("id",strokeNumber);
+		    lastX = x;
+		    lastY = y;
+			$(document).mouseup(function(){
+		    	mousedown = false;
+			});
+			$('svg').mousemove(function(event){
+		    	if (!mousedown) {
+		        	return;
+		    	}
+			    var x = event.pageX,
+			        y = event.pageY;
+		    	pathString += 'L' + x + ',' + y; 
+		    	$("#"+strokeNumber).attr({"d":pathString});
+			});
+		});
+		
+	//shape tool - rectangle
+	/*
+
+		var mousedown = false;
+		$('svg').mousedown(function(event){
+		var startX, startY, shape;
+
+			event.preventDefault(); //cursor fix
+			strokeNumber++;
+		    mousedown = true;
+		    var x = event.pageX,
+		        y = event.pageY;
+
+			shape = paper.rect(x,y,10,10).attr({"fill": fillColor, "stroke-linecap": "round", "stoke-linejoin": "round", stroke: strokeColor, "stroke-width": strokeWidth});	
+	    	$('rect:not([id])').attr("id",strokeNumber);
+
+		    startX = x;
+		    startY = y;
+
+			$(document).mouseup(function(){
+		    	mousedown = false;
+			});
+
+			$('svg').mousemove(function(event){
+		    	if (!mousedown) {
+		        	return;
+		    	}
+			    var x = event.pageX,
+			        y = event.pageY;
+
+		    	$("#"+strokeNumber).attr({"d":pathString});
+			});
+		});
+	*/
+
 });
